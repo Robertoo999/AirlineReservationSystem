3f35b6cdc3a4843722e05ef8874c3c001f851b25
Robert Orzełowski <roorzel77@wp.pl>
2023-04-25 21:17:17 +0200

    Added values to enum and because of it i could removed a lot of boiled code in class Airplane

--- src/Airplane.java
+++ src/Airplane.java
@@ -9,65 +9,31 @@ public class Airplane {
     private List<List<Seat>> seats;
 
 
-    public Airplane(int columns,int rows,Classes classes) {
-        this.rows=rows;
-        this.columns=columns;
-        this.classes=classes;
+    public Airplane(int columns, int rows, Classes classes) {
+        this.rows = rows;
+        this.columns = columns;
+        this.classes = classes;
         seats = new ArrayList<>();
     }
 
-    public void fillSeats()
-    {
+    public void fillSeats() {
+        for (int i = 0; i < rows; i++) {
+            seats.add(new ArrayList<>());
+            for (int j = 0; j < columns; j++) {
 
-                switch(this.classes)
-                {
-                    case FIRST -> {
-                        for (int i = 0; i < rows; i++) {
-                            seats.add(new ArrayList<>());
-                            for (int j = 0; j < columns; j++) {
-
-                        if (j == 0 || j == columns-1)
-                            seats.get(i).add(new Seat(30 + 5, i + 1, j + 1)) ;
-                        else
-                            seats.get(i).add(new Seat(30, i + 1, j + 1)) ;
-                    }}}
-
-                    case SECOND -> {
-                        for (int i = 0; i < rows; i++) {
-                            seats.add(new ArrayList<>());
-                            for (int j = 0; j < columns; j++) {
-
-                                if (j == 0 || j == columns-1)
-                                    seats.get(i).add(new Seat(20 + 5, i + 1, j + 1)) ;
-                                else
-                                    seats.get(i).add(new Seat(20, i + 1, j + 1)) ;
-                            }}
-                    }
-
-                    case THIRD -> {
-                        for (int i = 0; i < rows; i++) {
-                            seats.add(new ArrayList<>());
-                            for (int j = 0; j < columns; j++) {
-
-                                if (j == 0 || j == columns-1)
-                                    seats.get(i).add(new Seat(10 + 5, i + 1, j + 1)) ;
-                                else
-                                    seats.get(i).add(new Seat(10, i + 1, j + 1)) ;
-                            }}
-                    }
-                }
-
+                if (j == 0 || j == columns - 1)
+                    seats.get(i).add(new Seat(this.classes.prize+5, i + 1, j + 1));
+                else
+                    seats.get(i).add(new Seat(this.classes.prize, i + 1, j + 1));
             }
+        }
+    }
 
 
-
-    public void showSeats()
-    {
-        for (List<Seat> s: seats)
-        {
-            for (Seat ss : s)
-            {
-                System.out.print(ss+" ");
+    public void showSeats() {
+        for (List<Seat> s : seats) {
+            for (Seat ss : s) {
+                System.out.print(ss + " ");
             }
             System.out.println();
         }
@@ -75,10 +41,6 @@ public class Airplane {
 
     public int getRows() {
         return rows;
-    }
-
-    public void setRows(int rows) {
-        this.rows = rows;
     }
 
     public int getColumns() {
@@ -99,5 +61,6 @@ public class Airplane {
 
     public List<List<Seat>> getSeats() {
         return seats;
-    }}
+    }
+}
 
--- src/Classes.java
+++ src/Classes.java
@@ -1,6 +1,9 @@
 public enum Classes {
-    FIRST
-    ,SECOND
-    ,THIRD;
-
+    FIRST(30),
+    SECOND(20),
+    THIRD(10);
+    int prize;
+    Classes(int prize) {
+        this.prize=prize;
+    }
 }
